c62f0c36cf07461483ecd17982933e4f1d117f63
Hrishi Mukherjee <hrishimukherjee25@outlook.com>
2026-05-11 22:06:52 -0400

    Updated port.

--- app.py
+++ app.py
@@ -274,4 +274,10 @@ def regenerate(dream_id: int):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
